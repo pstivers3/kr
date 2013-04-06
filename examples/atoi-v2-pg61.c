cf8562409064917ab08adaed1d42e0f214eb3014
Paul Stivers <pstivers3@comcast.net>
2013-04-06 00:20:16 -0700

    first

---
+++ examples/atoi-v2-pg61.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+// atoi: convert s to integer; version 2 
+int atoi(char s[]) {
+  int i, n, sign;
+  for (i = 0; isspace(s[i]); i++) // skip white space
+    ;
+  sign = (s[i] == '-') ? -1 : 1;
+  if (s[i] == '+' || s[i] == '-') // skip sign
+    i++;
+  for (n = 0; isdigit(s[i]); i++) {
+    n = 10 * n + (s[i] - '0');
+    printf("n%d = %d\n", i, n);
+  }
+  return sign * n;
+}
+
+void main() {
+    char s[20] = "821";
+    printf("%d\n",atoi(s));
+}
+
